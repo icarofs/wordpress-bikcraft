c9b8bc081205a233350962fd3ef6981e8e6eb20b
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2021-03-24 17:00:11 -0300

    WIP - adicionado função para pegar img e corrigido caminho do link da imagem

--- header.php
+++ header.php
@@ -19,14 +19,14 @@
 		<script src="js/libs/modernizr.custom.45655.js"></script>
     
     <!-- Inicio Wordpress Header  -->
-    <?php wp_head(   ); ?>
+    <?php wp_head(); ?>
 	</head>
 	<body>
 
 		<header class="header">
 			<div class="container">
-				<a href="/index/" class="grid-4">
-					<img src="img/bikcraft.png" alt="Bikcraft">
+				<a href="/" class="grid-4">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/bikcraft.png" alt="Bikcraft">
 				</a>
 				<nav class="grid-12 header_menu">
 					<ul>
