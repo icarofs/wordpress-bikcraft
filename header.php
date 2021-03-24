731666ac13e0894d1611197eae462c6cbd3eb191
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2021-03-24 16:45:24 -0300

    WIP - corrigindo caminho dos links do header

--- header.php
+++ header.php
@@ -25,15 +25,15 @@
 
 		<header class="header">
 			<div class="container">
-				<a href="index.html" class="grid-4">
+				<a href="/index/" class="grid-4">
 					<img src="img/bikcraft.png" alt="Bikcraft">
 				</a>
 				<nav class="grid-12 header_menu">
 					<ul>
-						<li><a href="sobre.html">Sobre</a></li>
-						<li><a href="produtos.html">Produtos</a></li>
-						<li><a href="portfolio.html">Portfólio</a></li>
-						<li><a href="contato.html">Contato</a></li>
+						<li><a href="/sobre/">Sobre</a></li>
+						<li><a href="/produtos/">Produtos</a></li>
+						<li><a href="/portfolio/">Portfólio</a></li>
+						<li><a href="/contato/">Contato</a></li>
 					</ul>
 				</nav>
 			</div>
