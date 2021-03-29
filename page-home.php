4c1b3db082808607cb626dcfefd56ce966a63fb0
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2021-03-29 11:20:01 -0300

    WIP - criado includes para reduzir código na aplicação

--- page-home.php
+++ page-home.php
@@ -56,11 +56,7 @@ get_header();
 <section class="portfolio">
 	<div class="container">
 		<h2 class="subtitulo">Portfólio</h2>
-		<div class="portfolio_lista">
-			<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-			<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-			<div class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-		</div>
+	<?php include(TEMPLATEPATH . "/include/clientes-portfolio.php") ?>
 		<div class="call">
 			<p><?php the_field('chamada_portfolio') ?></p>
 			<a href="/portfolio/" class="btn">Portfólio</a>
@@ -68,28 +64,9 @@ get_header();
 	</div>
 </section>
 
-<section class="qualidade container">
-	<h2 class="subtitulo">Qualidade</h2>
-	<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
-	<ul class="qualidade_lista">
-		<li class="grid-1-3">
-			<h3>Durabilidade</h3>
-			<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
-		</li>
-		<li class="grid-1-3">
-			<h3>Design</h3>
-			<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-		</li>
-		<li class="grid-1-3">
-			<h3>Sustentabilidade</h3>
-			<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-		</li>
-	</ul>
-	<div class="call">
-		<p>conheça mais a nossa história</p>
-		<a href="sobre.html" class="btn btn-preto">Sobre</a>
-	</div>
-</section>
+
+<?php include(TEMPLATEPATH . "/include/qualidade.php") ?>
+
 <?php endwhile; else: endif; ?>
 
 <?php get_footer(); ?>
